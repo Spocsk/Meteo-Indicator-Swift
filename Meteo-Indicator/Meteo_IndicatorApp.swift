8918508d6e456e008922b5271ef03129f5a0d095
Dylan COUTO DE OLIVEIRA <dylandasilva79@gmail.com>
2025-09-06 12:36:15 +0200

    Initial Commit

---
+++ Meteo-Indicator/Meteo_IndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meteo_IndicatorApp.swift
+//  Meteo-Indicator
+//
+//  Created by Dylan COUTO DE OLIVEIRA on 06/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Meteo_IndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Meteo-IndicatorTests/Meteo_IndicatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Meteo_IndicatorTests.swift
+//  Meteo-IndicatorTests
+//
+//  Created by Dylan COUTO DE OLIVEIRA on 06/09/2025.
+//
+
+import Testing
+@testable import Meteo_Indicator
+
+struct Meteo_IndicatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
